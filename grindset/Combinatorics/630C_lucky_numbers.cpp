68967e3bfbe38d4acc708827f334f064bc63dbdd
Agnideep Ghosh <95609274+ag-wnl@users.noreply.github.com>
2024-01-28 10:30:18 +0530

    Update 630C_lucky_numbers.cpp

--- grindset/Combinatorics/630C_lucky_numbers.cpp
+++ grindset/Combinatorics/630C_lucky_numbers.cpp
@@ -22,3 +22,17 @@ int main(){
 
     cout << ans << "\n";
 }
+
+//Better Runtime code almost O(1):
+// As 16 = 10000, 15 = 01111 -> which has all powers of 2 added (from 0 to n-1) -> so answer = 2^(n+1) - 1 - 1(removing 2^0);
+int main(){
+    ios_base::sync_with_stdio(false), cin.tie(nullptr);
+ 
+    int n;
+    cin >> n;
+    
+    ll ans = 1LL<<(n+1);
+    ans -= 2;
+ 
+    cout << ans << "\n";
+}
